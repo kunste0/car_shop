5a67586b41dbbd5e82677317ba40d3a45b6ab6a6
steffen <kunste0@outlook.de>
2023-02-08 22:08:57 +0100

    Fix wrong argument

--- src/main/java/com/example/car_shop/dto/mapper/ArticleDTOMapper.java
+++ src/main/java/com/example/car_shop/dto/mapper/ArticleDTOMapper.java
@@ -2,10 +2,9 @@ package com.example.car_shop.dto.mapper;
 
 import com.example.car_shop.dto.ArticleDTO;
 import com.example.car_shop.model.Article;
-import com.example.car_shop.model.ArticleType;
 
 public class ArticleDTOMapper {
     public ArticleDTO mapModelToDto(Article article) {
-        return new ArticleDTO(article.getArticleNumber(), article.getName(), article.getPrice(), article.getArticleNumber());
+        return new ArticleDTO(article.getArticleNumber(), article.getName(), article.getPrice(), article.getArticleType().getId());
     }
 }
