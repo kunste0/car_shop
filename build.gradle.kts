e7632bdfbe39bc5428eb091d7ba94dda06279171
steffen <kunste0@outlook.de>
2023-02-14 18:09:25 +0100

    Add swagger documentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
 	implementation("org.projectlombok:lombok")
     testImplementation("junit:junit:4.13.1")
 	testImplementation("org.testng:testng:7.1.0")
